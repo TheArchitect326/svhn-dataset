2dd0f3e246349ac49cf0b2c137b13f3d3231ab15
TheArchitect326 <seftontycho@hotmail.co.uk>
2020-04-26 16:10:25 +0100

    ewgf

---
+++ rename.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+
+max = '0'
+for png in glob(os.path.join(os.getcwd(), 'images/train/*.png')):
+  this = png.split('.')[0].split('/')[-1]
+  if int(this) > int(max):
+    max = this
+
+print(max)
+
+for png in glob(os.path.join(os.getcwd(), 'images/test/*.png')):
+  name = png.split('.')[0].split('/')[-1] 
+  newname = str(int(name) + int(max))
+  os.rename(png, f'images/test/{newname}.png')
+
+max = '0'
+for xml in glob(os.path.join(os.getcwd(), 'annotations/train/*.xml')):
+  this = xml.split('.')[0].split('/')[-1]
+  if int(this) > int(max):
+    max = this
+
+print(max)
+
+for xml in glob(os.path.join(os.getcwd(), 'annotations/test/*.xml')):
+  name = xml.split('.')[0].split('/')[-1]  
+  newname = str(int(name) + int(max))
+  os.rename(xml, f'annotations/test/{newname}.png')
--- xml_to_csv.py
+++ xml_to_csv.py
@@ -4,20 +4,23 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 
 
-def xml_to_csv(path):
+def xml_to_csv(path, adj=0):
     xml_list = []
-    for xml_file in glob.glob(path + '/*.xml'):
+    print(path)
+    print(os.path.exists(path))
+    for xml_file in glob.glob(os.path.join(path, '*.xml')):
+        print(xml_file)
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-            value = (root.find('filename').text,
+            value = (str(int(root.find('filename').text.split('.')[0]) + adj) + '.png',
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
                      member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
+                     int(member[1][0].text),
+                     int(member[1][1].text),
+                     int(member[1][2].text),
+                     int(member[1][3].text)
                      )
             xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
@@ -26,9 +29,10 @@ def xml_to_csv(path):
 
 
 def main():
-    for i in ['train','test']:
-        image_path = os.path.join(os.getcwd(), 'images/{}'.format(i))
-        xml_df = xml_to_csv(image_path)
+    for c, i in enumerate(['test', 'train']):
+        image_path = os.path.join(os.getcwd(), 'annotations/{}'.format(i))
+        print(image_path)
+        xml_df = xml_to_csv(image_path, c * 33402)
         xml_df.to_csv('data/{}_labels.csv'.format(i), index=None)
 
     print('Successfully converted xml to csv.')
